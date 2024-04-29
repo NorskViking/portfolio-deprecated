782f319df62fad0d4ee0a372613290bd73985cec
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-04-29 12:50:26 +0200

    Create volunteerData.js
    Started on new section for volunteer experience.

---
+++ src/data/volunteerData.js
@@ -0,0 +1,32 @@
+export const volunteerData = [
+    {
+        id: 1,
+        title: "PitCrew - Inferno Metal Festival 2024",
+        description: "",
+        image: inferno
+    },
+    {
+        id: 2,
+        title: "DeltaForce - Midgardsblot Metal Festival 2023",
+        description: "",
+        image: midgardsblot
+    },
+    {
+        id: 3,
+        title: "Environment - Tons of Rock Festival 2023",
+        description: "",
+        image: tonsOfRock
+    },
+    {
+        id: 4,
+        title: "Pre-Prod - Midgardsblot Metal Festival 2022",
+        description: "",
+        image: midgardasblot
+    },
+    {
+        id: 5,
+        title: " - Inferno Metal Festival",
+        description: "",
+        image: inferno
+    },
+]
