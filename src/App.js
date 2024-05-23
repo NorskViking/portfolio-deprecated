bc12f9ffc98792e9aaefe66ae28415cbbdf217c4
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-05-23 09:33:44 +0200

    Removed unused code
    Removed some unused code, testing if github actions deploys correctly.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
 import MainPage from './pages/Main/MainPage'
-import ProjectPage from './pages/Project/ProjectPage'
-import ScrollToTop from './components/ScrollToTop/ScrollToTop'
-import BackToTop from './components/BackToTop/BackToTop'
-
-import Footer from './components/Footer/Footer';
 
 import { Routes, Route } from 'react-router-dom';
-import list from './assets/svg/list.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container'
 import ScrollToTopButton from './components/ScrollToTop/ScrollToTop';
-
-console.log(list)
 
 export default function App() {
   return (
